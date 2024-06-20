eb2c8a64ece833fc9adbd411ed70f16c03be522d
Alexey Kapralov <alewka24@gmail.com>
2024-06-21 00:37:21 +0300

    User entity, service, repo was created,

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,22 @@ import { AppService } from './app.service';
 import { UsersController } from './features/users/api/users.controller';
 import { UsersService } from './features/users/application/users.service';
 import { UsersRepository } from './features/users/infrastructure/users.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './features/users/domain/user.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService, UsersRepository],
+    imports: [
+        MongooseModule.forRoot('mongodb+srv://admin:6XeshSKaryTj@cluster0.n2nwife.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+            dbName: 'nest-test'
+        }),
+        MongooseModule.forFeature([
+            {
+                name: User.name,
+                schema: UserSchema,
+            },
+        ]),
+    ],
+    controllers: [AppController, UsersController],
+    providers: [AppService, UsersService, UsersRepository],
 })
-
-export class AppModule {
-}
+export class AppModule {}
